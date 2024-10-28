b9d086591b6883dac0b59e68588632f247e59af5
shubham kumari <kumarishubham273@gmail.com>
2024-10-28 09:29:45 +0530

    useReducer

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react"
+import logoImg from "../assets/logo.jpg"
+import Button from "./UI/Button"
+import CartContext from "../store/CartContext"
+export default function Header(){
+    const {items} = useContext(CartContext)
+    const totalCartItems = items.reduce((totalNumberOfItems, item) => {
+        return totalNumberOfItems + item.quantity
+    }, 0)
+    return(
+        <header id="main-header">
+            <div id="title">
+                <img src={logoImg} alt="A restaurant" />
+                <h1>ReactFood</h1>
+            </div>
+            <nav>
+                <Button textOnly>cart ({totalCartItems})</Button>
+            </nav>
+        </header>
+    )
+}
