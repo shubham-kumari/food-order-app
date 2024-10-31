49c415a7ef6b5f2b5beed30ea942b826fb9ab336
shubham kumari <kumarishubham273@gmail.com>
2024-10-31 12:18:35 +0530

    custom hook

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,20 +1,29 @@
-import { useEffect, useState } from "react";
+import useHttp from "../hooks/useHttp";
 import MealItem from "./MealItem";
 
+const requestConfig = {}
+
 export default function Meals(){
-    const [loadedMeals, setLoadedMeals] = useState([])
-    useEffect(() => {
-        async function fetchMeals(){
-            const response = await fetch('http://localhost:3000/meals')
-            if(!response.ok){
-                // ...
-            }
-            const meals = await response.json();
-            setLoadedMeals(meals);
-        }
-        fetchMeals();
-    }, [])
+    // const [loadedMeals, setLoadedMeals] = useState([])
+    // useEffect(() => {
+    //     async function fetchMeals(){
+    //         const response = await fetch('http://localhost:3000/meals')
+    //         if(!response.ok){
+    //             // ...
+    //         }
+    //         const meals = await response.json();
+    //         setLoadedMeals(meals);
+    //     }
+    //     fetchMeals();
+    // }, [])
+
+    const {data : loadedMeals, isLoading, error} = useHttp('http://localhost:3000/meals', requestConfig , []);
+    console.log(loadedMeals);
     
+    if(isLoading){
+        <p className="center">Fetching meals...</p>
+    }
+
     return (
         // GET/meals
         <ul id="meals">{loadedMeals.map((meal) => 
